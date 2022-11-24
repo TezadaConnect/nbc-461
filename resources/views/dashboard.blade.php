7454c5d3f222c3c9f0b568d01c5207872b702eb4
Kenyleen Pan <kenyleenp@gmail.com>
2022-11-24 11:45:22 +0800

    Removing invention as to review for research coordinator

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,7 @@
                                     <h5 class="card-header text-center">Research Coord.</h5>
                                     <div class="card-body d-flex justify-content-center">
                                         @foreach ($department[10] as $value)
-                                            @include('dashboard.researcher', ['countToReview' => $countToReview[10][$value->cluster_id], 'collegeCode' => $value->cluster_name ])
+                                            @include('dashboard.researcher', ['countToReview' => $countToReview[10][$value->college_id], 'collegeCode' => $value->cluster_name ])
                                         @endforeach
                                     </div>
                                 </div>
