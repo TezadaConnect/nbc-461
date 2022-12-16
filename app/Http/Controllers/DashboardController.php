8d9765a29d7129304753d6a699e47a41c854a4ee
Kenyleen Pan <kenyleenp@gmail.com>
2022-12-16 20:59:44 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -178,6 +178,8 @@ class DashboardController extends Controller
             $countRegisteredUsers[6] = '';
             $countExpectedTotal[6] = '';
             $countReceived[6] = '';
+            $college[6] = "";
+            $college[12] = "";
 
             if (in_array(12, $roles)) {
                 $college[12] = College::join('associates', 'colleges.id', 'associates.college_id')->where('associates.user_id', auth()->id())->where('associates.deleted_at', null)->get();
@@ -219,6 +221,8 @@ class DashboardController extends Controller
             $countRegisteredUsers[7] = '';
             $countExpectedTotal[7] = '';
             $countReceived[7] = '';
+            $sector[7] = "";
+            $sector[13] = "";
 
             if (in_array(13, $roles)) {
                 $sector[13] = Associate::join('sectors', 'associates.sector_id', 'sectors.id')->where('associates.user_id', auth()->id())->where('associates.deleted_at', null)->get();
