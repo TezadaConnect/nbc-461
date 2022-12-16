ac3f3543898151a642829d8551b4598b73a9c7ac
Kenyleen Pan <kenyleenp@gmail.com>
2022-12-16 23:33:01 +0800

    HOT FIX: Reports to review display in associates/assistants - By 12/16/2022

--- app/Http/Controllers/Reports/ToReceive/DeanController.php
+++ app/Http/Controllers/Reports/ToReceive/DeanController.php
@@ -52,7 +52,7 @@ class DeanController extends Controller
         $department_list = collect();
         $currentQuarterYear = Quarter::find(1);
 
-        $officeCredential = $assignments[6]->concat($assignments[12]);
+        $officeCredential = collect($assignments[6]->concat($assignments[12]));
         foreach ($officeCredential as $row){
             $tempReports = Report::where('reports.report_year', $currentQuarterYear->current_year)
                 // ->where('reports.report_quarter', $currentQuarterYear->current_quarter)
