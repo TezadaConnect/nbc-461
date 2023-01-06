22bc834dc699301bd949012b359a3bde4a7d2e48
kenpenndmnl <kenyleenp@gmail.com>
2023-01-06 11:11:53 +0800

    Update DeanController.php

--- app/Http/Controllers/Reports/ToReceive/DeanController.php
+++ app/Http/Controllers/Reports/ToReceive/DeanController.php
@@ -52,7 +52,7 @@ class DeanController extends Controller
         $department_list = collect();
         $currentQuarterYear = Quarter::find(1);
 
-        $officeCredential = collect($assignments[6])->merge($assignments[12])->unique();
+        $officeCredential = collect($assignments[6]);
         foreach ($officeCredential as $row){
             $tempReports = Report::where('reports.report_year', $currentQuarterYear->current_year)
                 // ->where('reports.report_quarter', $currentQuarterYear->current_quarter)
